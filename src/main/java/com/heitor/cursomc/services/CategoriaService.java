d7957a72fb83480c862ad697f386a0252f03cc51
Heitor Teixeira <heitoret@gmail.com>
2018-09-26 22:45:26 -0300

    Validação sintatica com bean validation

--- src/main/java/com/heitor/cursomc/services/CategoriaService.java
+++ src/main/java/com/heitor/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.heitor.cursomc.domain.Categoria;
+import com.heitor.cursomc.dto.CategoriaDTO;
 import com.heitor.cursomc.repositories.CategoriaRepository;
 import com.heitor.cursomc.services.exceptions.DataIntegrityException;
 import com.heitor.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,8 @@ public class CategoriaService {
 		return repo.findAll(pageRequest);
 	}
 	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
 	
 }
