170d0a7f3bf41ef8200a96176264435b60ed8e16
Heitor Teixeira <heitoret@gmail.com>
2018-10-03 21:17:35 -0300

    put delete e get para cliente

--- src/main/java/com/heitor/cursomc/services/ClienteService.java
+++ src/main/java/com/heitor/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.heitor.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.heitor.cursomc.domain.Cliente;
+import com.heitor.cursomc.dto.ClienteDTO;
 import com.heitor.cursomc.repositories.ClienteRepository;
+import com.heitor.cursomc.services.exceptions.DataIntegrityException;
 import com.heitor.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,5 +28,36 @@ public class ClienteService {
 							+ id + ", Tipo: " + Cliente.class.getName()));
 	}
 	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma cliente porque existem entidades relacionadas");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
 	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction) , orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
 }
